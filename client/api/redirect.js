126b34939ee3efcb184979da35d1f83b85aa8cd1
Mark Morcos <mark.yehia@gmail.com>
2024-01-29 00:01:42 +0100

    bugfix: redirect function

--- client/api/redirect.js
+++ client/api/redirect.js
@@ -1,11 +1,11 @@
-import { Router } from "next/router";
+import Router from "next/router";
 
 export default function ({ context, path }) {
   if (typeof window === "undefined") {
     context.res.writeHead(302, { Location: path });
     context.res.end();
   } else {
-    Router.redirect(path);
+    Router.replace(path);
   }
 
   return {};
