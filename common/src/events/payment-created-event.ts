7ecf0855998bc719b335abd6299bc58d5dbf28ab
Mark Morcos <mark.yehia@gmail.com>
2024-02-14 15:34:52 +0100

    feat: migrate from stripe to paypal

--- common/src/events/payment-created-event.ts
+++ common/src/events/payment-created-event.ts
@@ -2,5 +2,5 @@ import { Subjects } from "./subjects";
 
 export interface PaymentCreatedEvent {
   subject: Subjects.PaymentCreated;
-  data: { id: string; orderId: string; stripeId: string };
+  data: { id: string; orderId: string; paypalOrderId: string };
 }
